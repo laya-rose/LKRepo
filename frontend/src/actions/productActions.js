48e286abd4443ce96564ea65087d1a71c7d296f9
Laya Rose Pallipuram <roselaya98@gmail.com>
2021-10-13 17:11:14 +0530

    set currentPage =1 in getProducts func also path change of getProducts

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -9,10 +9,10 @@ import {
     PRODUCT_DETAILS_FAIL
 } from '../constants/productConstants'
 
-    export const getProducts=()=>async(dispatch)=>{
+    export const getProducts=(currentPage=1)=>async(dispatch)=>{
         try {
            dispatch({type:ALL_PRODUCTS_REQUEST}) 
-           const {data}=await axios.get('/api/v1/products')
+           const {data}=await axios.get(`/api/v1/products?page=${currentPage}`)
            dispatch({
                type:ALL_PRODUCTS_SUCCESS,
                payload:data
