2c8bc68bb88718bac2772b6af9eb31c9efa8ce5b
Laya Rose Pallipuram <roselaya98@gmail.com>
2021-10-15 14:33:37 +0530

    added keyword

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -9,10 +9,10 @@ import {
     PRODUCT_DETAILS_FAIL
 } from '../constants/productConstants'
 
-    export const getProducts=(currentPage=1)=>async(dispatch)=>{
+    export const getProducts=(keyWord='',currentPage=1)=>async(dispatch)=>{
         try {
            dispatch({type:ALL_PRODUCTS_REQUEST}) 
-           const {data}=await axios.get(`/api/v1/products?page=${currentPage}`)
+           const {data}=await axios.get(`/api/v1/products?keyword=${keyWord}&page=${currentPage}`)
            dispatch({
                type:ALL_PRODUCTS_SUCCESS,
                payload:data
